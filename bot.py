4106252f8dfa4ea87598b42bc3c437eb654bfea3
Joseph Martinez <39905973+NotJoeMartinez@users.noreply.github.com>
2019-05-01 18:58:31 -0500

    Detect_lables function recives uri from reddit mention

--- bot.py
+++ bot.py
@@ -15,37 +15,28 @@ reddit = praw.Reddit(client_id='my client id',
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'path to local json file'
 
 
-# Path to local things folder
-things_folder = 'Path to folder with unkown images'
-
-# download image
-def download_images(url, file_path, file_name):
-    full_path = file_path + file_name + '.jpg'
-    urllib.request.urlretrieve(url, full_path)
-
-
 # pull images from reddit
-def get_images():
-    for submission in reddit.subreddit('whatisthisthing').rising(limit=5):
-        download_images(submission.url, things_folder, submission.id)
-        print("submission with id " + submission.id + " saved to things folder")
+def get_uri():
+    for message in reddit.inbox.mentions():
+        return message.parent().url
 
-# detects lables in file and prints them to console
-def detect_labels(path):
+
+# detect image lables
+def detect_labels(uri):
+    """Detects labels in the file located in Google Cloud Storage or on the
+    Web."""
+    from google.cloud import vision
     client = vision.ImageAnnotatorClient()
-    with io.open(path, 'rb') as image_file:
-        content = image_file.read()
-
-    image = vision.types.Image(content=content)
+    image = vision.types.Image()
+    image.source.image_uri = uri
 
     response = client.label_detection(image=image)
     labels = response.label_annotations
     print('Labels:')
-
     for label in labels:
         print(label.description)
 
+# todo: reply to the comment with the lables
 
-# calls detect_lables function on local directory
-for image in os.listdir(things_folder):
-    detect_labels(things_folder + image)
+
+detect_labels(get_uri())
