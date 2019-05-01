6467ca7ff6665c895f0059dd0074004af1998053
Joseph Martinez <39905973+NotJoeMartinez@users.noreply.github.com>
2019-04-30 20:15:25 -0500

    Added cloud vision api
    The detect_lables() is called on every image in the things_folder directory and returns the lables generated by google cloud vision api.

--- bot.py
+++ bot.py
@@ -1,9 +1,21 @@
+import io
+import os
 import praw
 import urllib.request
+from google.cloud import vision
 
-# Praw authentication
+
+# Reddit praw authentication
+reddit = praw.Reddit(client_id='my client id',
+                     client_secret='my client secret',
+                     user_agent='my user agent')
 
 
+# Google vision authentication
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'path to local json file'
+
+
+# Path to local things folder
 things_folder = 'Path to folder with unkown images'
 
 # download image
@@ -18,4 +30,22 @@ def get_images():
         download_images(submission.url, things_folder, submission.id)
         print("submission with id " + submission.id + " saved to things folder")
 
-get_images()
+# detects lables in file and prints them to console
+def detect_labels(path):
+    client = vision.ImageAnnotatorClient()
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.types.Image(content=content)
+
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+    print('Labels:')
+
+    for label in labels:
+        print(label.description)
+
+
+# calls detect_lables function on local directory
+for image in os.listdir(things_folder):
+    detect_labels(things_folder + image)
